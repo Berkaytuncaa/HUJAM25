84a2626bead6809f76b99138526cbd31a418dc34
Tuna Eskan <tunaeskanaytac@gmail.com>
2025-10-31 15:25:42 +0300

    Create tamplate

---
+++ Assets/Scripts/DressUIController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DressUIController : MonoBehaviour
+{
+    public void OnTopButtonPressed()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
